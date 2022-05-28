78141231714375dd99411ad2730af1d996b973d0
Mateo <madiazt@unal.edu.co>
2022-05-28 13:48:53 -0500

    added skeleton of app router

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+
+require("dotenv").config();
+
+let port = process.env.PORT || 3000;
+
+// routes
+const apiRouterV1 = require("./routes/router.js")
+
+const app = express();
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// conect mongodb
+
+// routes handler
+app.use("/apiv1", apiRouterV1);
+
+app.use((err, req, res) => {
+    console.log(err);
+    if (!res.headersSent) {
+        res.status(err.httpStatusCode || 500).render('UnknownError');
+    };
+});
+
+
+app.listen(port, () => {
+    console.log(`api listening on port ${port}`);
+});
---
+++ routes/router.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+// controllers
+const createUser = require("../controllers/createUser");
+const getUser = require("../controllers/getUser");
+const editUser = require("../controllers/editUser");
+const deleteUser = require("../controllers/deleteUser");
+
+// middlewares
+
+// routes
+router.post("/portfolio");
+router.get("/portfolio");
+router.put("/portfolio");
+router.delete("/portfolio");
+
+module.exports = router
