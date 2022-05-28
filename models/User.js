76e696f9e491fc75ddd2d3d974ffdffbaa387ddd
Mateo <madiazt@unal.edu.co>
2022-05-28 13:40:28 -0500

    added user model component

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    firstName: {
+        type: String, required: true, minLength: 3, maxLength: 15
+    },
+    secondName: {
+        type: String, required: true, minLength: 3, maxLength: 15
+    }, 
+    title: {
+        type: String, required: true, minLenght: 3, maxLength: 25
+    },
+    description: {
+        type: String, required: true, minLength: 15, maxLength: 400
+    },
+    imageURL: {
+        type: String, required: true
+    }
+});
+
+const User = moongose.model("User", userSchema);
+module.exports = User;
