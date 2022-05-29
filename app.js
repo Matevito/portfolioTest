2d7dcc2baa1d5417c9c9d512be455e40beb9e10d
Mateo <madiazt@unal.edu.co>
2022-05-28 22:50:43 -0500

    added cors to api

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const compression = require("compression");
+const helmet = require("helmet");
+const cors = require("cors");
 
 require("dotenv").config();
 
@@ -9,6 +12,9 @@ const apiRouterV1 = require("./routes/router.js")
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(compression());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
